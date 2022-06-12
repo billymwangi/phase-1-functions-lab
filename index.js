d3184adbef5e279049e0ef79c84e108d0a3134b1
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-12 21:55:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    let blocks = 0
+    if (distance< 42);
+    return (42-34)
+function distanceFromHqInBlocks(num){
+       let blocks = 0;
+        num < 42 ? blocks = 42 - num : blocks = num - 42;
+        return blocks; 
+    }
